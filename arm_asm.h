e30931f203520511e516dbe5d7d61a3181d40765
Harm Hanemaaijer <fgenfb@yahoo.com>
2013-06-06 00:26:42 +0200

    Add preload variants for block write=16

--- arm_asm.h
+++ arm_asm.h
@@ -10,7 +10,22 @@ extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16(void *dest,
 extern void *memcpy_armv5te_no_overfetch_align_32_block_write_8(void *dest,
     const void *src, int n);
 
-extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16(void *dest,
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_64(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_96(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_128(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_160(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_192(void *dest,
+    const void *src, int n);
+
+extern void *memcpy_armv5te_no_overfetch_align_32_block_write_16_preload_256(void *dest,
     const void *src, int n);
 
 extern void *memcpy_armv5te_no_overfetch_align_32_block_write_32_preload_64(void *dest,
