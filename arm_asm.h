00a2a710a882b30230d3389dc22b57ece37d7d4d
Harm Hanemaaijer <fgenfb@yahoo.com>
2013-06-08 00:14:32 +0200

    Add variant with write alignment of 16 and (early) preload offset of 128

--- arm_asm.h
+++ arm_asm.h
@@ -10,6 +10,9 @@ extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16(void *dest,
 extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16_preload_early_96(void *dest,
     const void *src, int n);
 
+extern void *memcpy_armv5te_no_overfetch_align_16_block_write_16_preload_early_128(void *dest,
+    const void *src, int n);
+
 extern void *memcpy_armv5te_no_overfetch_align_32_block_write_8(void *dest,
     const void *src, int n);
 
